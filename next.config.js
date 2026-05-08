aee26b10596dba2ede1bf40b3653ca53ede59899
Sachin Pandey <yogathing0@gmail.com>
2026-05-08 12:10:12 +0545

    fix: rename next.config.ts to next.config.js

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  // Optional: Improve compatibility with Netlify
+  output: 'standalone',
+};
+
+module.exports = nextConfig;
